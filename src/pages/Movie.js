4f58438587d399e18ce582e44b73af3eb07c864a
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-08-02 17:25:50 +0200

    add a naviguate button

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,14 +1,31 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Movies from "../Movies.json";
+import { useNavigate } from "react-router-dom";
 
 const Movie = () => {
   const params = useParams();
   const [movie, setMovie] = useState(Movies[params.id - 1]);
+  const navigate = useNavigate();
+  const handleButtonClick = () => {
+    navigate("/");
+  };
 
   return (
     <>
       <img src={movie.image} alt={movie.title} />
+      <p>{movie.title}</p>
+      <p>{movie.director}</p>
+      <p>{movie.description}</p>
+      <br />
+      {movie.stars.map((star) => {
+        return (
+          <ul>
+            <li>{star}</li>
+          </ul>
+        );
+      })}
+      <button onClick={handleButtonClick}>Back to home</button>
     </>
   );
 };
